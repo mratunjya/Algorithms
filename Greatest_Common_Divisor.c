a387ccd91469609dc6b66dd9b3c611f98be4d1b5
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2022-10-10 23:03:38 +0530

    Update Greatest_Common_Divisor.c

--- Greatest_Common_Divisor.c
+++ Greatest_Common_Divisor.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
-{  int n1,n2,t,i;
-   printf("Enter The Two Numbers \n");
-   scanf("%d%d",&n1,&n2);
-   if(n2>n1)
-   {t = n1;
-    n1= n2;
-    n2 = t;}
-    for(i = n2;i>0;i--)
-    {if(n1%i==0&&n2%i==0)
-    {  printf("%d \n",i);
-        break;}
-}
+{
+    int n1, n2, t, i;
+    printf("Enter The Two Numbers \n");
+    scanf("%d%d", &n1, &n2);
+    if (n2 > n1)
+    {
+        t = n1;
+        n1 = n2;
+        n2 = t;
+    }
+    for (i = n2; i > 0; i--)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            printf("%d \n", i);
+            break;
+        }
+    }
 }
